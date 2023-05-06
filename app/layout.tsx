34208c0f7a2e8c9d2dda310e9413f5ca2ca3b9a2
themx05 <maximiliencomlan05@gmail.com>
2023-05-06 20:08:54 +0100

    fix: restructured layout and roots

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,78 @@
-import RootLayout from "./root"
+/* eslint-disable @next/next/next-script-for-ga */
+import RootLayout from "./root";
 
 export const metadata = {
   title: {
     default: "CodeSanctum - A Safe Sanctuary for Learning to Code",
-    template: "CodeSanctum - %s"
+    template: "CodeSanctum - %s",
   },
-  description: "Join CodeSanctum and embark on a journey to learn to code with the help of our expert mentors and gamified approach. No prior experience required.",
+  description:
+    "Join CodeSanctum and embark on a journey to learn to code with the help of our expert mentors and gamified approach. No prior experience required.",
   robots: {
-    index: true, 
-    follow: true
+    index: true,
+    follow: true,
   },
-  keywords: ["CodeSanctum", "learn to code", "coding", "gamification", "mentoring"],
+  keywords: [
+    "CodeSanctum",
+    "learn to code",
+    "coding",
+    "gamification",
+    "mentoring",
+  ],
   openGraph: {
     type: "website",
     url: "https://www.codesanctum.org/",
     title: "CodeSanctum - A Safe Sanctuary for Learning to Code",
-    description: "Join CodeSanctum and embark on a journey to learn to code with the help of our expert mentors and gamified approach. No prior experience required.",
+    description:
+      "Join CodeSanctum and embark on a journey to learn to code with the help of our expert mentors and gamified approach. No prior experience required.",
     images: [
       {
         url: "https://www.codesanctum.com/images/hero/sanctuary.png",
         width: 1200,
         height: 630,
-        alt: "CodeSanctum landing page image"
-      }
-    ]
-  }
-}
-
+        alt: "CodeSanctum landing page image",
+      },
+    ],
+  },
+};
 
 export default function BaseLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <RootLayout>
-      {children}
-    </RootLayout>
-  )
+    <html suppressHydrationWarning lang="en">
+      {/*
+    <head /> will contain the components returned by the nearest parent
+    head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+  */}
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VK3LE19SRX"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WVFJLKJ');`,
+          }}
+        />
+      </head>
+      <body className="dark:bg-black">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WVFJLKJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        <RootLayout>{children}</RootLayout>
+      </body>
+    </html>
+  );
 }
--- app/root.tsx
+++ app/root.tsx
@@ -3,11 +3,11 @@
 import ScrollToTop from "@/components/ScrollToTop";
 import "node_modules/react-modal-video/css/modal-video.css";
 import { Providers } from "./providers";
-import ReactModal from "react-modal";
 import { Toaster } from "react-hot-toast";
 
 import "../styles/index.css";
 import RootContent from "./content";
+import ReactModal from "react-modal";
 
 ReactModal.setAppElement("body");
 
@@ -17,41 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html suppressHydrationWarning lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-VK3LE19SRX"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-WVFJLKJ');`,
-          }}
-        />
-      </head>
-      <body className="dark:bg-black">
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WVFJLKJ"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        <Providers>
-          <RootContent>{children}</RootContent>
-          <Toaster position="top-center" />
-          <ScrollToTop />
-        </Providers>
-      </body>
-    </html>
+    <Providers>
+      <RootContent>{children}</RootContent>
+      <Toaster position="top-center" />
+      <ScrollToTop />
+    </Providers>
   );
 }
