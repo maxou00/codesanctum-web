d5b0ae3d7a7b87fac146a145617c30f6ee70147b
themx05 <maximiliencomlan05@gmail.com>
2023-05-15 12:49:35 +0100

    fix: updated google keys

--- core/google.ts
+++ core/google.ts
@@ -1,3 +1,5 @@
+import { byEnv } from "./env";
+
 export const googleClient = {
-    clientId: "733023448743-fj3aleckjv79niimdfi9a4l1gacmouf6.apps.googleusercontent.com"
+    clientId: byEnv("733023448743-hut78eor2tbct1oqqd3sbef1qd2pnb0e.apps.googleusercontent.com", "733023448743-fj3aleckjv79niimdfi9a4l1gacmouf6.apps.googleusercontent.com")
 }
